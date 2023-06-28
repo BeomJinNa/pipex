a9a7440fc0601eb8faf097efc55190a17f02984b
Beomjin Na <bena@c10r8s1.42seoul.kr>
2023-06-28 18:57:26 +0900

    FIX: Fix main function to check args

--- srcs/init_data.c
+++ srcs/init_data.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 12:20:07 by bena              #+#    #+#             */
-/*   Updated: 2023/06/28 17:48:19 by bena             ###   ########.fr       */
+/*   Updated: 2023/06/28 18:49:55 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,30 @@
 
 char	*get_path(char **ep);
 
+#include <stdio.h>//TEST
 int	init_pipex(t_data *data, int ac, char **av, char **ep)
 {
+	printf("init start\n");//TEST
 	data->number_of_cmds = ac - 3;
 	data->cmds = &av[2];
 	data->ep = ep;
+	printf("get_path\n");//TEST
 	data->path = get_path(ep);
 	if (data->path == NULL)
 		return (-1);
+	printf("start malloc\n");//TEST
 	data->pid = (pid_t *)malloc(sizeof(pid_t) * data->number_of_cmds);
 	if (data->pid == NULL)
 		return (clean_malloc(data));
+	printf("open files\n");//TEST
 	if (open_infile(&data->infile, av[1]))
 		return (clean_mallocs(data));
 	if (open_outfile(&data->outfile, av[ac - 1]))
 		return (clean_infile(data));
+	printf("create pipes\n");//TEST
 	data->pipe = create_pipes(data->number_of_cmds - 1);
 	if (data->pipe == NULL)
 		return (clean_inout(data));
+	printf("initialization end\n");//TEST
 	return (0);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 21:38:49 by bena              #+#    #+#             */
-/*   Updated: 2023/06/28 17:39:51 by bena             ###   ########.fr       */
+/*   Updated: 2023/06/28 18:47:57 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,24 @@ void		wait_child_processes(t_data *data);
 static int	pipex(int ac, char **av, char **ep);
 static int	pipex_bonus(int ac, char **av, char **ep);
 static int	return_error(void);
+static void	print_usage(void);
 
 int	main(int ac, char **av, char **ep)
 {
-	if (ft_strncmp("here_doc", av[1], 9))
+	if (ac >= 2)
 	{
-		if (pipex(ac, av, ep))
-			return (return_error());
+		if (ft_strncmp("here_doc", av[1], 9))
+		{
+			if (ac >= 5)
+				if (pipex(ac, av, ep))
+					return (return_error());
+		}
+		else
+			if (ac >= 6)
+				if (pipex_bonus(ac, av, ep))
+					return (return_error());
 	}
-	else
-		if (pipex_bonus(ac, av, ep))
-			return (return_error());
+	print_usage();
 	return (0);
 }
 
@@ -40,16 +47,20 @@ static int	pipex(int ac, char **av, char **ep)
 {
 	t_data	data;
 
+	printf("pipex launched\n"); //TEST
 	if (init_pipex(&data, ac, av, ep))
 		return (-1);
+	printf("data initialized\n"); //TEST
 	if (alloc_process(&data))
 		return (clean_all(&data));
+	printf("processes allocated\n"); //TEST
 	wait_child_processes(&data);
 	remove_pipes(data.pipe, data.number_of_cmds);
 	close(data.infile);
 	close(data.outfile);
 	free(data.pid);
 	free(data.path);
+	printf("end pipex\n"); //TEST
 	return (0);
 }
 
@@ -65,4 +76,14 @@ static int	return_error(void)
 {
 	perror("Error");
 	return (-1);
+}
+
+static void	print_usage(void)
+{
+	ft_printf("Usage: ./pipex %s %s %s %s %s\n",
+		"{<infile> or \"here_doc\" LIMITER}",
+		"<command1>",
+		"<command2>",
+		"...",
+		"<outfile>");
 }
